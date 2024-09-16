1ab0658fec2b0c66cb5129bcbe30456ab8c0e131
Storm <Storm@STORMSSD>
2024-09-17 00:40:41 +0300

    Add project files.

---
+++ ForwardList/ForwardList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForwardList
+{
+	class ForwardList
+	{
+		Element Head;
+		public ForwardList()
+		{
+			Head = null;
+			Console.WriteLine($"LConstructor:\t{GetHashCode()}");
+		}
+		~ForwardList()
+		{
+			Console.WriteLine($"LDestructor:\t{GetHashCode()}");
+		}
+		public void push_front(int Data)
+		{
+			Head = new Element(Data) { pNext = Head };
+		}
+		public void Print()
+		{
+			Element Temp = Head; // Temp это итератор.
+			while (Temp != null)
+			{
+				Console.Write($"{Temp.Data}\t");
+				Temp = Temp.pNext; // Переход на следующий элемент.
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ ForwardList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForwardList
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random rand = new Random(0);
+			Console.Write("Введите размер списка");
+			int n = Convert.ToInt32(Console.ReadLine());
+			ForwardList List = new ForwardList();
+			for (int i = 0; i < n; i++)
+			{
+				List.push_front(rand.Next(100));
+			}
+			List.Print();
+		
+		}
+	}
+}
